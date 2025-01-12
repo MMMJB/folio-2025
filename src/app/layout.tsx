1656d41f7c4dfa76fbe15e4c103196c75d8f2861
MMMJB <michaelbeck445@gmail.com>
2025-01-12 02:13:28 -0500

    chore: add fonts

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,14 +1,24 @@
 import "../styles/reset.css";
 import "../styles/globals.css";
 
+import { Instrument_Serif } from "next/font/google";
+
+const instrumentSerif = Instrument_Serif({
+  subsets: ["latin"],
+  weight: ["400"],
+});
+
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body>{children}</body>
+    <html
+      lang="en"
+      className={`text-text h-full bg-background ${instrumentSerif.className}`}
+    >
+      <body className="h-full">{children}</body>
     </html>
   );
 }
