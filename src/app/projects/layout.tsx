db03b5a20d4e75b362da1319d67a998e82cd3c46
MMMJB <michaelbeck445@gmail.com>
2025-02-04 02:11:31 -0500

    feat: make blog responsive

--- src/app/projects/layout.tsx
+++ src/app/projects/layout.tsx
@@ -6,7 +6,7 @@ import Link from "next/link";
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <div className="size-full overflow-y-auto">
-      <article className="mx-auto max-w-screen-md p-12 pt-24">
+      <article className="mx-auto max-w-screen-md px-6 md:p-12 md:pt-24">
         {children}
       </article>
       <div className="mx-auto max-w-screen-md px-12 pb-12">
--- src/components/Figure/index.tsx
+++ src/components/Figure/index.tsx
@@ -1,6 +1,6 @@
 export default function Figure({ children }: { children: React.ReactNode }) {
   return (
-    <figure className="flex flex-col">
+    <figure className="flex flex-col overflow-auto">
       <span className="flex items-center justify-end gap-1 px-1 pb-2 text-xs">
         <div className="relative">
           <div className="size-1.5 rounded-full bg-green-500" />
